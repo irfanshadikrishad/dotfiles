33e56802d1447294a9a778963290cf9110909b0f
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-06-26 09:07:54 +0600

    seperate files for plugins

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/oil.nvim",
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = false,
+    view_options = { show_hidden = true },
+    float = { padding = 10 },
+    delete_to_trash = true,
+  },
+  lazy = false,
+}
---
+++ nvim/.config/nvim/lua/plugins/search-replace.lua
@@ -0,0 +1,9 @@
+return {
+  "roobert/search-replace.nvim",
+  config = function()
+    require("search-replace").setup {
+      default_replace_single_buffer_options = "gcI",
+      default_replace_multi_buffer_options = "egcI",
+    }
+  end,
+}
