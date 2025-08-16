d16b0f2000b4b3d2a4e703e5b0fd9052f54eb51f
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2025-08-16 06:42:48 +0600

    diagnostics plugin

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -2,8 +2,12 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- Basic mappings
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>", { desc = "Save file" })
 map("i", "jk", "<ESC>")
+
+-- Floaterm toggle
 map("n", "<leader>ft", function()
   local ok, floaterm = pcall(require, "floaterm")
   if ok then
@@ -11,16 +15,21 @@ map("n", "<leader>ft", function()
   end
 end, { noremap = true, silent = true, desc = "Toggle floaterm." })
 
+-- Oil file manager
 map("n", "<leader>-", ":Oil --float<CR>", { desc = "Opens Oil file manager in float mode." })
+
+-- CodeSnap mappings
 map("v", "<leader>0", ":CodeSnap<CR>", { desc = "Snaps the selected code to clipboard." })
-map("v", "<leader>1", ":CodeSnapSave<CR>", { desc = "Snaps and saves the selected code." }) -- optional
+map("v", "<leader>1", ":CodeSnapSave<CR>", { desc = "Snaps and saves the selected code." })
+
+-- Timerly toggle
 map("n", "<leader>tt", ":TimerlyToggle<CR>", { desc = "Toggle Timerly." })
 
+-- Move lines up/down
 -- Normal mode: Move current line up/down
-vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
-vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
--- Visual mode: Move selected block up/down
-vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv", { desc = "Move block up", silent = true })
-vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv", { desc = "Move block down", silent = true })
+map("n", "<M-Up>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
+map("n", "<M-Down>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Visual mode: Move selected block up/down
+map("v", "<M-Up>", ":m '<-2<CR>gv=gv", { desc = "Move block up", silent = true })
+map("v", "<M-Down>", ":m '>+1<CR>gv=gv", { desc = "Move block down", silent = true })
