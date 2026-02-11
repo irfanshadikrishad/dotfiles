5ff2cbb4d302ec686d7b9d13ff1586522b7357af
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2026-02-12 02:00:03 +0600

    chore(dotfiles): auto backup on 2026-02-12 02:00:01

---
+++ nvim/.config/nvim/lua/plugins/cord.lua
@@ -0,0 +1,16 @@
+return {
+  "vyfor/cord.nvim",
+  enabled = true,
+  editor = {
+    client = "neovim",
+    tooltip = "The Superior Text Editor",
+    icon = nil,
+  },
+  display = {
+    theme = "default",
+    flavor = "dark",
+    view = "full",
+    swap_fields = false,
+    swap_icons = false,
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/mywpm.lua
@@ -0,0 +1,35 @@
+return {
+  "slowy07/mywpm.nvim",
+  configs = function()
+    require("mywpm").setup {
+      -- interval
+      notify_interval = 60 * 1000,
+      -- highest wpm
+      high = 60,
+      -- lowest wpm
+      low = 15,
+
+      -- highest wpm message
+      high_msg = "nice keep it up 🔥",
+      -- lowest wpm message
+      low_message = "hahaha slowhand 🐌",
+
+      -- show notify and virtual text
+      show_virtual_text = true,
+      notify = true,
+      -- interval update time
+      update_time = 300,
+      -- overriding function to showing format
+      virt_wpm = function(wpm)
+        return ("👨‍💻 Speed: %.0f WPM"):format(wpm)
+      end,
+      -- position of virtual wpm position
+      -- available `eol`, `right_align`, `inline`
+      virt_wpm_pos = "right_align",
+
+      -- follow the cursor when insert mode
+      -- default (false)
+      follow_cursor = true,
+    }
+  end,
+}
