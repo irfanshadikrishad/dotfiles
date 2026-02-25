9beb18da74976e5f8afcd20403456b18a93a16c8
Irfan Shadik Rishad <77362427+irfanshadikrishad@users.noreply.github.com>
2026-02-25 18:00:04 +0600

    chore(dotfiles): auto backup on 2026-02-25 18:00:01

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,18 @@
 return {
   "github/copilot.vim",
-  lazy = false, -- load on startup
+  lazy = false,
   event = "InsertEnter",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+
+    local map = vim.keymap.set
+
+    map("i", "<M-]>", "<Plug>(copilot-next)")
+    map("i", "<M-[>", "<Plug>(copilot-previous)")
+
+    map("i", "<M-l>", 'copilot#Accept("<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+  end,
 }
